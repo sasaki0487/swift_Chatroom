f64d751208283290cbea184aaa4cfdff0640d6d5
sasaki0487 <34514493+sasaki0487@users.noreply.github.com>
2018-12-17 13:05:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var socketArray = [];
-var name = [];
+var name = []; //儲存各個client名字的array
 
 //設定從網頁連進Server時，回傳index.html給瀏覽器 (瀏覽器端為Client，index.html裡面有client的code)
 app.get('/',
@@ -22,84 +22,81 @@ http.listen(portNumber,
 
 //可將 io 視為 Server 上管理所有 Socket 的 Manager
 io.on('connection',
-      function(socket) /*[1]*/
-      {
-      
-      //將新創造的 Socket加到 socketArray 裡
-      socketArray[socketArray.length] = socket;
-      //在 server 的 console 中輸出現有的 socket 數量量
-      console.log('socket count: '+socketArray.length);
-        socket.on('user send out message', /*[2]*/
-                  function(msg)
-                  {
-                  
-                  //only send message back to the client of this socket ( with event string 'show message on screen' )
-                  //socket.emit('show message on screen', msg);
-                  
-                  //broadcast to everyone include the client of this socket ( with event string 'event string' ), you can define 'event string' your self
-                  io.emit('show message on screen', msg);
-                  
-                  //broadcast to everyone except for the client of this socket ( with event string 'event string' ), you can define 'event string' your self
-                  //socket.broadcast.emit('show message on screen', 'other: ' + msg);
-                  
-                  });
-        socket.on('register', /*[2]*/
-                function(msg)
+    function(socket) /*[1]*/
+    {
+        //將新創造的 Socket加到 socketArray 裡
+        socketArray[socketArray.length] = socket;
+        //在 server 的 console 中輸出現有的 socket 數量量
+        console.log('socket count: '+socketArray.length);
+        socket.on('user send out message', /*接收到要傳給其他client的訊息*/
+            function(msg)
+            {   
+                //broadcast to everyone include the client of this socket ( with event string 'event string' ), you can define 'event string' your self
+                io.emit('show message on screen', msg);
+            });
+        socket.on('register', /*有新的socket連上時登錄到name array裡*/
+            function(msg)
+            {
+                name[socketArray.length-1] = msg;
+                console.log('name: '+name[socketArray.length-1]);
+                /*把傳送對象的名字後面加上 "<-" 後用/n串成一串emit給該socket，並在傳送後改回原本的名字*/
+                for(var i = 0 ; i < socketArray.length ; i++)
                 {
-                    name[socketArray.length-1] = msg;
-                    console.log('name: '+name[socketArray.length-1]);
-                  
-                    for(var i = 0 ; i < socketArray.length ; i++){
-                        var temp = name[i];
-                        name[i] = temp + "<-";
-                        var str = name.join('\n');  
-                        socketArray[i].emit('name', str);
-                        name[i] = temp;
-                    }
-                });
-      socket.on('private', /*[2]*/
-                function(msg)
+                    var temp = name[i];
+                    name[i] = temp + "<-";
+                    var str = name.join('\n');
+                    socketArray[i].emit('name', str);
+                    name[i] = temp;
+                }
+            });
+        socket.on('disconnect', /* client按斷線時會接收到此訊息 */
+            function()
+            {
+                /* 尋找斷開的socketindex */
+                var socketIndex;
+                for(var i = 0 ; i < socketArray.length ; i++)
                 {
-                    var strArray = msg.split(':');
-                    var rec=-1,send;
-                    for(var i = 0 ; i < name.length ; i++){
-                        if(name[i] == strArray[0]){
+                    if(socketArray[i] == socket)
+                        socketIndex = i;
+                }
+                /*更新socket array跟name array*/
+                socketArray.splice(socketIndex,1);
+                name.splice(socketIndex,1);
+                console.log('socket count: '+socketArray.length);
+                /*把更新後的name list傳送給各個client*/
+                for(var i = 0 ; i < socketArray.length ; i++)
+                {
+                    var temp = name[i];
+                    name[i] = temp + "<-";
+                    var str = name.join('\n');
+                    socketArray[i].emit('name', str);
+                    name[i] = temp;
+                }
+              });
+    
+         socket.on('private', /*私訊功能*/
+            function(msg)
+            {
+                /*接收到的msg格式為 "$name:$message" ，先split分開*/
+                var strArray = msg.split(':');
+                /*預設接收者的index為-1，以免使用者輸入錯誤造成server出現error*/
+                var rec=-1,send;
+                /*從接收到的傳送對象名字搜尋其index*/
+                for(var i = 0 ; i < name.length ; i++)
+                {
+                    if(name[i] == strArray[0])
                         rec = i;
-                        }
-                    }
-                    for(var i = 0 ; i < socketArray.length ; i++){
-                        if(socketArray[i] == socket)
-                            send = i;
-                    }
-                    var out = name[send] + ':' + strArray[1]
-                    socketArray[send].emit('show message on screen', out);
-                    if(rec!=-1)
-                        socketArray[rec].emit('show message on screen', out);
-                });
-        socket.on('disconnect',
-                  function()
-                  {
-                    var socketIndex;
-                    for(var i = 0 ; i < socketArray.length ; i++){
-                        if(socketArray[i] == socket)
-                            socketIndex = i;
-                    }
-                    socketArray.splice(socketIndex,1);
-                    name.splice(socketIndex,1);
-                    console.log('socket count: '+socketArray.length);
-                      for(var i = 0 ; i < socketArray.length ; i++){
-                          var temp = name[i];
-                          name[i] = temp + "<-";
-                          var str = name.join('\n');  
-                          socketArray[i].emit('name', str);
-                          name[i] = temp;
-                      }
-                  });
+                }
+                /*同一份訊息也要顯示在sender自己的message裡面，因此搜尋自己的index*/
+                for(var i = 0 ; i < socketArray.length ; i++){
+                    if(socketArray[i] == socket)
+                        send = i;
+                }
+                /*輸出訊息為 "$sender_name:" */
+                var out = name[send] + ':' + strArray[1]
+                socketArray[send].emit('show message on screen', out);
+                /*如果有找到傳送對象的話就emit，否則不emit*/
+                if(rec!=-1)
+                   socketArray[rec].emit('show message on screen', out);
+            });
       });
-
-/*
-[1] when a new clinet connect to server, server will deploy a new socket to handle the connection to this client and call this function with the new created socket as parameter.
-
-[2] tells socket to handle 'chat message from clinet' event. when socket get 'chat message frome clinet' event, server will call following function and the event message as the parameter
- 
-*/
